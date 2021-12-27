9b3584dc3c3af9d9f6b1ece2838528eaa4431c9a
root <Infinity>
2021-12-27 10:29:24 +0800

    first

---
+++ README.md
@@ -0,0 +1,55 @@
+Welcome to the Infinity.... 
+<*)))))><
+<*)))))><
+<*)))))>< 
+INFO[2021-11-26T09:28:34+08:00] version: 0.5.3-dev                           
+WARN[2021-11-26T09:28:34+08:00] clef is not enabled; portability and security of your keys is sub optimal 
+INFO[2021-11-26T09:28:34+08:00] using existing infinity network address: 2b7933223dceb8ebcc0f0023f22ae2e922f84d5dd66321b187de0c103dd94453 
+INFO[2021-11-26T09:28:34+08:00] infinity public key 021d9e61480e42621ccc3fce4da7d35051f41707e75b21cc1c908da3404c7b0392 
+INFO[2021-11-26T09:28:34+08:00] pss public key 0388476827c23b00ec9af8c7ca53f4006ee04d2972f1cc5b7bc04bb32df46f3010 
+INFO[2021-11-26T09:28:34+08:00] using infinity address 4e6da3d9526340a81876533d16a6e136d82eec71 
+INFO[2021-11-26T09:28:34+08:00] debug api address: [::]:1635                 
+INFO[2021-11-26T09:28:35+08:00] using custom factory address: ac61e8f921b10452dbfaa66ac833d3a522e61290 
+INFO[2021-11-26T09:28:35+08:00] using existing chequebook 5a78d561d4bf07bab2501fe2c8b34249f2b52c4f 
+File not found.
+
+2021/11/26 09:28:36 failed to sufficiently increase receive buffer size (was: 208 kiB, wanted: 2048 kiB, got: 416 kiB). See https://github.com/lucas-clemente/quic-go/wiki/UDP-Receive-Buffer-Size for details.
+WARN[2021-11-26T09:28:49+08:00] invalid bootnode address /18.183.82.121/p2p/16Uiu2HAmT2rcJJfaAyjFyLQecosEmCyAmSUWoEZBceig3rQmTP4v 
+INFO[2021-11-26T09:28:49+08:00] database capacity: 5000000 chunks (approximately 19.5GB) 
+INFO[2021-11-26T09:28:49+08:00] name resolver: no name resolution service provided 
+INFO[2021-11-26T09:28:49+08:00] api address: [::]:1633                       
+compute cpu reward according to the following cpu information:4e6da3d9526340a81876533d16a6e136d82eec71
+Name: Intel(R) Xeon(R) CPU E5-2686 v4 @ 2.30GHz
+PhysicalCores: 2
+ThreadsPerCore: 1
+LogicalCores: 2
+Family 6 Model: 79
+Features: CMOV,NX,MMX,MMXEXT,SSE,SSE2,SSE3,SSSE3,SSE4.1,SSE4.2,AVX,AVX2,FMA3,F16C,BMI1,BMI2,LZCNT,POPCNT,AESNI,CLMUL,RDRAND,ERMS,RDTSCP,CX16
+Cacheline bytes: 64
+L1 Data Cache: 32768 bytes
+L1 Instruction Cache: 32768 bytes
+L2 Cache: 262144 bytes
+L3 Cache: 47185920 bytes
+We have Streaming SIMD Extensions
+The score of CPU is: 18d19800
+File not found.
+
+
+
+
+
+compute cpu reward according to the following cpu information:4e6da3d9526340a81876533d16a6e136d82eec71
+Name: Intel(R) Xeon(R) CPU E5-2686 v4 @ 2.30GHz
+PhysicalCores: 2
+ThreadsPerCore: 1
+LogicalCores: 2
+Family 6 Model: 79
+Features: CMOV,NX,MMX,MMXEXT,SSE,SSE2,SSE3,SSSE3,SSE4.1,SSE4.2,AVX,AVX2,FMA3,F16C,BMI1,BMI2,LZCNT,POPCNT,AESNI,CLMUL,RDRAND,ERMS,RDTSCP,CX16
+Cacheline bytes: 64
+L1 Data Cache: 32768 bytes
+L1 Instruction Cache: 32768 bytes
+L2 Cache: 262144 bytes
+L3 Cache: 47185920 bytes
+We have Streaming SIMD Extensions
+The score of CPU is: 18d19800
+File not found.
---
+++ pkg/settlement/swap/cpuaward/cpuaward.go
@@ -0,0 +1,160 @@
+// Copyright 2020 The Infinity Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cpuaward
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	externalip "github.com/glendc/go-external-ip"
+	"github.com/klauspost/cpuid"
+	"github.com/yanhuangpai/voyager/pkg/settlement/swap/erc20"
+	"github.com/yanhuangpai/voyager/pkg/settlement/swap/transaction"
+)
+
+const ()
+
+var ()
+
+// Service is the main interface for interacting with the nodes chequebook.
+type Service interface {
+	GetIfi()
+}
+
+type service struct {
+	lock               sync.Mutex
+	transactionService transaction.Service
+
+	ownerAddress common.Address
+
+	erc20Service erc20.Service
+
+	initNum *big.Int
+}
+
+// New creates a new chequebook service for the provided chequebook contract.
+func NewCPUAward(transactionService transaction.Service, ownerAddress common.Address) (Service, error) {
+
+	return &service{
+		transactionService: transactionService,
+		ownerAddress:       ownerAddress,
+		initNum:            big.NewInt(0),
+	}, nil
+}
+
+// Compute returns the score of current device's CPU
+func (s *service) Compute() {
+	ticker := time.NewTicker(time.Second * 60)
+	go func() {
+		for _ = range ticker.C {
+			tip1 := fmt.Sprintf("compute cpu award according to the following cpu information:%x", s.ownerAddress)
+			println(tip1)
+			score, _, _ := CPUScore()
+			tip2 := fmt.Sprintf("The score of CPU is: %x", score)
+			println(tip2)
+			url := fmt.Sprintf("http://web.ifichain.com:8080/irc20/send_ifi?address=0x%x&amount=%x", s.ownerAddress, score)
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				continue
+			}
+			res, _ := http.DefaultClient.Do(req)
+			body, _ := ioutil.ReadAll(res.Body)
+			fmt.Println(string(body))
+		}
+	}()
+}
+
+func (s *service) GetIfi() {
+	ticker := time.NewTicker(time.Second * 60)
+	go func() {
+		for _ = range ticker.C {
+			tip1 := fmt.Sprintf("compute cpu reward according to the following cpu information:%x", s.ownerAddress)
+			println(tip1)
+			score, cpuName, _ := CPUScore()
+			tip2 := fmt.Sprintf("The score of CPU is: %x", score)
+			println(tip2)
+			consensus := externalip.DefaultConsensus(nil, nil)
+			// Get your IP,
+			// which is never <nil> when err is <nil>.
+			ip, err := consensus.ExternalIP()
+			if err != nil {
+				fmt.Println(ip.String()) // print IPv4/IPv6 in string format
+				continue
+			}
+			url := "http://web.ifichain.com:8080/irc20/get_ifi"
+			song := make(map[string]interface{})
+			song["owner_address"] = s.ownerAddress
+			song["cpu_score"] = score
+			song["local_ip"] = ip.String()
+			song["cpu_name"] = cpuName
+			song["status"] = 1
+			bytesData, err := json.Marshal(song)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			reader := bytes.NewReader(bytesData)
+			req, err := http.NewRequest("POST", url, reader)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			req.Header.Set("Content-Type", "application/json;charset=UTF-8")
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			body, _ := ioutil.ReadAll(res.Body)
+			fmt.Println(string(body))
+
+		}
+	}()
+}
+
+// CPUScore returns the score of current device's CPU
+func CPUScore() (score int, cpuName string, err error) {
+	// Print basic CPU information:
+	fmt.Println("Name:", cpuid.CPU.BrandName)
+	fmt.Println("PhysicalCores:", cpuid.CPU.PhysicalCores)
+	fmt.Println("ThreadsPerCore:", cpuid.CPU.ThreadsPerCore)
+	fmt.Println("LogicalCores:", cpuid.CPU.LogicalCores)
+	fmt.Println("Family", cpuid.CPU.Family, "Model:", cpuid.CPU.Model)
+	fmt.Println("Features:", cpuid.CPU.Features)
+	fmt.Println("Cacheline bytes:", cpuid.CPU.CacheLine)
+	fmt.Println("L1 Data Cache:", cpuid.CPU.Cache.L1D, "bytes")
+	fmt.Println("L1 Instruction Cache:", cpuid.CPU.Cache.L1D, "bytes")
+	fmt.Println("L2 Cache:", cpuid.CPU.Cache.L2, "bytes")
+	fmt.Println("L3 Cache:", cpuid.CPU.Cache.L3, "bytes")
+
+	// Test if we have a specific feature:
+	if cpuid.CPU.SSE() {
+		fmt.Println("We have Streaming SIMD Extensions")
+	}
+
+	score = (3 + cpuid.CPU.PhysicalCores + cpuid.CPU.LogicalCores) * cpuid.CPU.ThreadsPerCore * (cpuid.CPU.CacheLine*100000 + cpuid.CPU.Cache.L1D*100 + cpuid.CPU.Cache.L2*10 + cpuid.CPU.Cache.L3) * 1000000
+	return score, cpuid.CPU.BrandName, nil
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
