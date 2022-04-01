f19deda4d408c395ab9c20e03167bae041c57981
jimmyoz <12707306@qq.com>
2022-04-01 10:30:58 +0800

    Update

--- version.go
+++ version.go
@@ -5,13 +5,13 @@
 package voyager
 
 var (
-	version = "0.5.3" // manually set semantic version number
+	version = "1.0.0" // manually set semantic version number
 	commit  string    // automatically set git commit hash
 
 	Version = func() string {
 		if commit != "" {
 			return version + "-" + commit
 		}
-		return version + "-dev"
+		return version + ""
 	}()
 )
